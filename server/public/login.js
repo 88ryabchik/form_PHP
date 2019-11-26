d1c1bf4972e7c7394459e16aff94880908fa6690
skillup <myEmail@example.com>
2019-11-26 21:37:36 +0200

    v33432

--- server/public/login.js
+++ server/public/login.js
@@ -1,54 +1,83 @@
 'use strict'
-var userName = document.querySelector('#username');
-var userNameError = document.querySelector('#username + .auth__error');
 
-var userEmail = document.querySelector('#useremail');
-var userEmailError = document.querySelector('#useremail + .auth__error');
+var formManager = {
 
-var userPhone = document.querySelector('#userphone');
-var userPhoneError = document.querySelector('#userphone + .auth__error');
+    name: document.querySelector('#username'),
+    nameError: document.querySelector('#username + .auth__error'),
+    email: document.querySelector('#useremail'),
+    emailError: document.querySelector('#useremail + .auth__error'),
+    phone: document.querySelector('#userphone'),
+    phoneError: document.querySelector('#userphone + .auth__error'),
+    password: document.querySelector('#userpass'),
+    passwordError: document.querySelector('#userpass + .auth__error'),
+    subscribe: document.querySelector('#usersubscribe'),
+    sendBtn: document.querySelector('#sendbtn'),
 
-var userPass = document.querySelector('#userpass');
-var userSubscribe = document.querySelector('#usersubscribe');
-
-var sendBtn = document.querySelector('#sendbtn');
-
-console.log(sendBtn);
-
-sendBtn.onclick = send;
-
-function send() {
     
-    var isError = false;
 
-    if (!(/^[а-яА-Яa-zA-Z0-9\-_\.]{2,25}$/i.test(userName.value))) {
-        userNameError.classList.remove('auth__error_hide');
-        userNameError.classList.add('auth__error_show');
+}
 
-        isError = true;
+
+
+
+
+
+
+
+
+
+
+
+formManager.valid=function() {
+
+    var isNotError = true;
+
+    if (!(/^[а-яА-Яa-zA-Z0-9\-_\.]{2,25}$/i.test(this.name.value))) {
+        this.nameError.classList.remove('auth__error_hide');
+        this.nameError.classList.add('auth__error_show');
+
+        isNotError = false;
     }
 
-    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i.test(userEmail.value))) {
-        userEmailError.classList.remove('auth__error_hide');
-        userEmailError.classList.add('auth__error_show');
+    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z]{1,10}$/i.test(this.email.value))) {
+        this.emailError.classList.remove('auth__error_hide');
+        this.emailError.classList.add('auth__error_show');
 
-        isError = true;
+        isNotError = false;
     }
 
-    if (!(/^(\+380|380|80|0){1,4}[0-9]{9}$/.test(userPhone.value))) {
-        userPhoneError.classList.remove('auth__error_hide');
-        userPhoneError.classList.add('auth__error_show');
+    if (!(/^(\+380|380|80|0){1,4}[0-9]{9}$/.test(this.phone.value))) {
+        this.phoneError.classList.remove('auth__error_hide');
+        this.phoneError.classList.add('auth__error_show');
 
-        isError = true;
+        isNotError = false;
     }
 
-    if(isError) return null;
+    if (!(/^[а-яА-Яa-zA-Z0-9\-_\.]{2,25}$/i.test(this.password.value))) {
+        this.passwordError.classList.remove('auth__error_hide');
+        this.passwordError.classList.add('auth__error_show');
+
+        isNotError = false;
+    }
+
+    return isNotError;
+
+
+
+}
+
+formManager.send = function () {
+
+    console.log(this)
+
+    if (this.valid() === false) return null;
 
     var data = {
-        name: userName.value,
-        password: userPass.value,
-        email: userEmail.value,
-        subscribe: userSubscribe.value,
+        name: this.name.value,
+        password: this.password.value,
+        phone: this.phone.value,
+        email: this.email.value,
+        subscribe: this.subscribe.value.checked,
     }
 
     fetch('/login', {
@@ -58,12 +87,12 @@ function send() {
 }
 
 
-function setClearHandler () {
+formManager.setClearHandler = function() {
     var elements = document.querySelectorAll('.auth__text');
 
-    elements.forEach(function(element){
-        
-        element.onclick = function(){
+    elements.forEach(function (element) {
+
+        element.onclick = function () {
             console.log(this.nextElementSibling);
             this.nextElementSibling.classList.remove('auth__error_show');
             this.nextElementSibling.classList.add('auth__error_hide');
@@ -71,4 +100,11 @@ function setClearHandler () {
     });
 
 }
-setClearHandler();
+
+
+formManager.init = function() {
+    this.sendBtn.onclick = this.send.bind(this);
+    this.setClearHandler();
+};
+
+formManager.init();
--- server/views/login.php
+++ server/views/login.php
@@ -3,24 +3,24 @@
                 <legend>Авторизация</legend>
                 <div class="auth__row">
                     <label for="username">User name</label>
-                    <input class="auth__text" type="text" id="username">
+                    <input value="Vasa" class="auth__text" type="text" id="username">
                     <i class="auth__error auth__error_hide ">Don't contain (_, {},$,0-9 lenght > 5 chars)</i>
                 </div>
                 <div class="auth__row">
                     <label for="useremail">User email</label>
-                    <input class="auth__text" type="text" id="useremail">
+                    <input value="Vasa@gmail.com" class="auth__text" type="text" id="useremail">
                     <i class="auth__error auth__error_hide ">Not valid email (example@gmail.com) </i>
                 </div>
 
                 <div class="auth__row">
                     <label for="userphone">User Phone</label>
-                    <input class="auth__text" type="text" id="userphone">
+                    <input value="0985423456" class="auth__text" type="text" id="userphone">
                     <i class="auth__error auth__error_hide ">Not valid phone (+380934555654) </i>
                 </div>
 
                 <div class="auth__row">
                     <label for="userpass">User password</label>
-                    <input class="auth__text" type="password" id="userpass">
+                    <input value="45433jhfsdjhf" class="auth__text" type="password" id="userpass">
                     <i class="auth__error auth__error_hide ">Must  contain (az, AZ, 0-9, lenght > 7 chars)</i>
                 </div>
 
